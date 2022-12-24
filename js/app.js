37674007d8a75699fd9dd4b3059c2c06c0447ab9
Toluwanimi <rjayjutolu419@gmail.com>
2022-12-24 13:40:25 +0100

    Add updateUI function

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
-// const cityForm = document.querySelector('form');
-// const card = document.querySelector('.card');
-// const details = document.querySelector('.details');
-// const img = document.querySelector('.time');
-// const icon = document.querySelector('.icon img');
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const img = document.querySelector('.time');
+const icon = document.querySelector('.icon img');
 
 // // update the ui
-// const updateUI = data => {
-//     const cityDets = data.cityDets;
-//     const weather = data.weather;
+const updateUI = data => {
+    const cityDets = data.cityDets;
+    const weather = data.weather;
 
-//     details.innerHTML = `
-//         <h5 class="my-3">${cityDets.EnglishName}</h5>
-//         <div class="my-3">${weather.WeatherText}</div>
-//         <div class="display-4 my-4">
-//             <span>${weather.Temperature.Metric.Value}</span>
-//             <span>&deg;C</span>
-//         </div>
-//     `;
-//     console.log(data);
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+    console.log(data);
 
-//     // day or night image
-//     const timeSrc = weather.IsDayTime ? './img/day.svg' : './img/night.svg';
-//     img.setAttribute('src', timeSrc);
+    // day or night image
+    const timeSrc = weather.IsDayTime ? './img/day.svg' : './img/night.svg';
+    img.setAttribute('src', timeSrc);
 
-//     // the weather icons
-//     const iconSrc = `./img/icons/${weather.WeatherIcon}.svg`;
-//     icon.setAttribute('src', iconSrc);
+    // the weather icons
+    const iconSrc = `./img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
 
-//     if(card.classList.contains('d-none')){
-//         card.classList.remove('d-none');
-//     }
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
 
-// }
+}
 
 // // updatem the city
 // const updateCity = async city => {
@@ -52,7 +52,7 @@
 //         .catch(err => console.log(err.message));
 
 //     // set local storage
-//     localStorage.setItem('city', city);
+//     // localStorage.setItem('city', city);
 // });
 
 // if(localStorage.getItem('city')){
